b7cda2257251192a727730716319588fc1973f70
DAVI SOUSA SOARES <166557628+SoaresDavidson@users.noreply.github.com>
2026-06-18 18:23:44 -0300

    first commit

---
+++ PetPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetPulse
+//
+//  Created by Turma02-10 on 18/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("hasdhnljasndbkjanbdljasdbnakj")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetPulse/PetPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetPulseApp.swift
+//  PetPulse
+//
+//  Created by Turma02-10 on 18/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PetPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
